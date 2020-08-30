099ccad01691f28cd37ee50f3e713e45592df7cb
AGbetaryal <920291780@qq.com>
2020-08-30 16:30:32 +0800

    two commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+<!--    <img src="./assets/logo.png">-->
+<!--    <router-view/>-->
+    <router-view/>
+    <!--<FooterGuider v-show="$route.meta.showFooter"/>-->
+  </div>
+</template>
+
+<script>
+  import FooterGuider from "./components/FooterGuider/FooterGuider"
+  import {mapActions} from 'vuex'
+  export default {
+    mounted () {
+      // this.$store.dispatch('getAddress')
+      this.getAddress()
+      this.getUserInfo()
+    },
+
+    methods: {
+      ...mapActions(['getAddress', 'getUserInfo'])
+    },
+    components: {
+      FooterGuider
+    }
+  /*  name: 'App'*/
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
